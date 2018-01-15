32bf16100d6e195ca16418adb70670117dce735e
Matt Groff <mattlgroff@gmail.com>
2018-01-15 14:54:24 -0800

    Takes input url from websocket instead of Environmental Variable

---
+++ controllers/cron.js
@@ -0,0 +1,21 @@
+const cron = require('node-cron');
+const diffDetector = require('./diffDetector.js');
+
+module.exports = {
+  seconds: function(url ,io, seconds){
+    cron.schedule(`*/${seconds} * * * * *`, function(){
+      console.log(`running diffDetector every ${seconds} seconds`);
+
+      diffDetector.isDiff(url, function(diff){
+        //Diffence Detected
+        if(diff){
+          io.emit('server response', `Change detected.`);
+        }
+        //No Difference Detected
+        else{
+          io.emit('server response', `No change detected.`);
+        }
+      });
+    });
+  }
+}
